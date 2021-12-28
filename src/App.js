83f6c6547355d7c8eef85accffccfe66a95eeb1a
David Harris <dharris@Davids-MBP.cgocable.net>
2021-12-27 23:10:15 -0500

    initial header component and logo, working on sign in/up page and reusable form components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
+import SignInSignUpPage from "./pages/si-su-pages/si-su.component";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInSignUpPage />} />
       </Routes>
     </div>
   );
