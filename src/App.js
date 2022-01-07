3135eed9cd503ad9f8dbbd379acc5acbd2f4dc64
David Harris <dharris@Davids-MBP.cgocable.net>
2022-01-07 12:24:43 -0500

    signin auth redirect, action interface file to store redux action values

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -41,15 +41,30 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/signin" element={<SignInSignUpPage />} />
+          <Route
+            path="/signin"
+            element={
+              <SignInWrapper currentUser={this.props.currentUser}>
+                <SignInSignUpPage />
+              </SignInWrapper>
+            }
+          />
         </Routes>
       </div>
     );
   }
 }
 
+const SignInWrapper = ({ children, currentUser }) => {
+  return currentUser ? <Navigate to="/" replace /> : children;
+};
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
