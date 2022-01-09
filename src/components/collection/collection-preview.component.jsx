4b96f5ed9bbafbf0485a1e92f4bd13e79f28fd9c
David Harris <dharris@Davids-MBP.cgocable.net>
2022-01-09 12:39:04 -0500

    cart item component and redux implemented for dropdown

--- src/components/collection/collection-preview.component.jsx
+++ src/components/collection/collection-preview.component.jsx
@@ -3,21 +3,19 @@ import "../collection-item/collection-item.component";
 import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.scss";
 
-const CollectionPreview = ({title, items}) => {
-  return(
+const CollectionPreview = ({ title, items }) => {
+  return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {
-          items
-            .filter((item, idx) => idx < 4)
-            .map(({id, ...otherItemProps}) => (
-              <CollectionItem key={id} {...otherItemProps}/>
-            ))
-        }
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
       </div>
     </div>
-  )
+  );
 };
 
 export default CollectionPreview;
